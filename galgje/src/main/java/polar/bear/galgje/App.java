180ac7f8b3076aad1c5d67f3de343887f1bb34e9
Michiel de Smet <Michieldesmet1994@gmail.com>
2018-12-09 18:40:42 +0100

    Setup of the galgje project

---
+++ galgje/src/main/java/polar/bear/galgje/App.java
@@ -0,0 +1,12 @@
+package polar.bear.galgje;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Ik moet even testen ;) ");
+	}
+}
