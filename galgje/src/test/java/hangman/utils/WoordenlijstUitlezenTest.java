3dc212857be9025682b369083449076ef59f7850
Michiel de Smet <Michieldesmet1994@gmail.com>
2020-03-05 12:36:26 +0100

    Changed the words playable to 60 and added English words as well

--- galgje/src/test/java/hangman/utils/WoordenlijstUitlezenTest.java
+++ galgje/src/test/java/hangman/utils/WoordenlijstUitlezenTest.java
@@ -10,23 +10,38 @@ import org.junit.Test;
 
 import hangman.utils.WordListReader;
 
-public class WoordenlijstUitlezenTest{
-	
+public class WoordenlijstUitlezenTest {
+
 	private Map<Integer, String> galgjeWoorden = new HashMap<>();
-	
+
 	@Test
-	public void testReadCSV4Words() throws IOException {
-		galgjeWoorden = new WordListReader().readsSelectedCSVFile("4_words_galgje.csv");
-		
-		assertEquals(339, galgjeWoorden.size());
-		assertEquals("foei", galgjeWoorden.get(100));
+	public void readCSV4WordsDutch() throws IOException {
+		galgjeWoorden = new WordListReader().readsSelectedCSVFile("4_words_dutch.csv");
+		assertEquals(60, galgjeWoorden.size());
+	}
+
+	@Test
+	public void readCSV5WordsDutch() throws IOException {
+		galgjeWoorden = new WordListReader().readsSelectedCSVFile("5_words_dutch.csv");
+		assertEquals(60, galgjeWoorden.size());
 	}
 	
 	@Test
-	public void testReadCSV5Words() throws IOException {
-		galgjeWoorden = new WordListReader().readsSelectedCSVFile("5_words_galgje.csv");
-		
-		assertEquals(445, galgjeWoorden.size());
-		assertEquals("elfje", galgjeWoorden.get(100));
+	public void readCsv4WordEnglish() throws IOException {
+		galgjeWoorden = new WordListReader().readsSelectedCSVFile("4_words_english.csv");
+		assertEquals(60, galgjeWoorden.size());
+	}
+	
+	@Test
+	public void readCsv5WordEnglish() throws IOException {
+		galgjeWoorden = new WordListReader().readsSelectedCSVFile("5_words_english.csv");
+		assertEquals(60, galgjeWoorden.size());
+	}
+	
+	private Map<Integer,String> createStubMap(){
+		for(int i =0; i < 60; i++) {
+			galgjeWoorden.put(i, "test_" + i);
+		}
+		return galgjeWoorden;
 	}
 }
